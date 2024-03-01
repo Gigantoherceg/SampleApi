a9ce75b9b8c4accba4c57c03ca1c62197310e15d
Gigantoherceg <Gigantoherceg@gmail.com>
2024-03-01 10:48:16 +0100

    Add scents.

--- SampleApi.Backend/Models/Product.cs
+++ SampleApi.Backend/Models/Product.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public ScentType Scent { get; set; }
         public string Description { get; set; } = string.Empty;
         public int Price { get; set; } = 0;
     }
---
+++ SampleApi.Backend/Models/ScentType.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleApiBackend.Models
+{
+    public enum ScentType
+    {
+        [Display(Name = "Rozmaring")]
+        Rozmaring,
+
+        [Display(Name = "Narancsvirág")]
+        Narancs,
+
+        [Display(Name = "Citromfű")]
+        Citrom,
+
+        [Display(Name = "Eukaliptusz")]
+        Eukaliptusz,
+
+        [Display(Name = "Mandula és méz")]
+        Mandula,
+
+        [Display(Name = "Szantálfa")]
+        Szantal,
+
+        [Display(Name = "Fenyő")]
+        Fenyo,
+
+        [Display(Name = "Rózsa")]
+        Rozsa,
+
+        [Display(Name = "Áfonya")]
+        Afonya,
+
+        [Display(Name = "Kávé")]
+        Kave,
+
+        [Display(Name = "Kókusz")]
+        Kokusz,
+
+        [Display(Name = "Gyömbér")]
+        Gyomberlime,
+
+        [Display(Name = "Fahéj")]
+        Fahej,
+
+        [Display(Name = "Levendula")]
+        Levendula,
+
+        [Display(Name = "Vanília")]
+        Vanilia,
+
+        [Display(Name = "Fűszernövények")]
+        Fuszeres,
+
+        [Display(Name = "Bergamott")]
+        Bergamott,
+
+        [Display(Name = "Mézeskalács")]
+        Mezeskalacs,
+
+        [Display(Name = "Kamilla")]
+        Kamilla,
+
+        [Display(Name = "Zöld sárkány")]
+        Lime
+
+    }
+}
